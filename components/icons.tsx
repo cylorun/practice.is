59a57c9c8915d4173a4f2605e70b9d55e72af96c
cylorun <allidabbi@gmail.com>
2025-01-28 13:57:31 +0000

    fix icon properties

--- components/icons.tsx
+++ components/icons.tsx
@@ -18,7 +18,7 @@ export const Icons = {
 	modes: (props: LucideProps) => <Dices {...props} />,
 	countries: (props: LucideProps) => (
 		<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none"
-			 stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"
+			 stroke="currentColor" stroke-width="3" strokeLinecap="round" strokeLinejoin="round"
 			 className="lucide lucide-earth">
 			<path d="M21.54 15H17a2 2 0 0 0-2 2v4.54"/>
 			<path d="M7 3.34V5a3 3 0 0 0 3 3a2 2 0 0 1 2 2c0 1.1.9 2 2 2a2 2 0 0 0 2-2c0-1.1.9-2 2-2h3.17"/>
