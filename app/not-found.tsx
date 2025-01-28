f104ab91595547dd284a277f6e7be926bb99b08c
cylorun <allidabbi@gmail.com>
2025-01-28 22:25:53 +0000

    not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+
+const NotFound = () => {
+	return (
+		<div className="flex h-screen flex-col items-center justify-center ">
+			<h1 className="text-6xl font-bold text-accent">404</h1>
+			<p className="mt-4 text-xl text-foreground">Þessi síða er ekki til.</p>
+		</div>
+	);
+};
+
+export default NotFound;
