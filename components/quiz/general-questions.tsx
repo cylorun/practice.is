13822c1fab13d469c3eba5752f2f12b263151d0d
cylorun <allidabbi@gmail.com>
2025-01-29 00:42:54 +0000

    way better answer checking, hraðspurningar for the general stuffs

--- components/quiz/general-questions.tsx
+++ components/quiz/general-questions.tsx
@@ -1,142 +1,250 @@
+
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
-
-
-import {capitalize, normalizeString} from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-
-
-
-
+import {capitalize, cn, normalizeString, stringDifference, stringPercentageMatch} from "@/lib/utils";
+import {Button, buttonVariants} from "@/components/ui/button";
 
 type Question = {
-	question: string
-	ans: string
-}
+	question: string;
+	ans: string;
+};
 
+type GameResult = {
+	question: string;
+	userAnswer: string;
+	correctAnswer: string;
+	score: number;
+	skipped: boolean;
+};
+
+const GAME_LENGTH_SECONDS = 90;
 
 const GeneralQuestions = () => {
 	const [questions, setQuestions] = useState<Question[]>([]);
-	const [randQuestion, setRandQuestion] = useState<Question>();
+	const [randQuestion, setRandQuestion] = useState<Question | null>(null);
 	const [isLoading, setIsLoading] = useState<boolean>(false);
-	const [guessTimes, setGuessTimes] = useState<number[]>([]);
-	const [startTime, setStartTime] = useState<number | null>(null);
-	const [averageTime, setAverageTime] = useState<number | null>(null);
-	const [roundsPlayed, setRoundsPlayed] = useState<number>(0);
-
 	const [userAnswer, setUserAnswer] = useState<string>("");
+	const [gameTime, setGameTime] = useState<number>(GAME_LENGTH_SECONDS);
+	const [roundsPlayed, setRoundsPlayed] = useState<number>(0);
+	const [gameOver, setGameOver] = useState<boolean>(false);
+	const [gameResults, setGameResults] = useState<GameResult[]>([]);
+	const [startTime, setStartTime] = useState<number>(Date.now());
+
 
 	const fetchQuestions = async () => {
-		setIsLoading(true)
+		setIsLoading(true);
 		try {
-			const res = await axios.get("/questions.json")
+			const res = await axios.get("/questions.json");
 			if (res.status === 200) {
-				setQuestions(res.data)
+				setQuestions(res.data);
 			} else {
-				throw new Error("Failed to fetch questions")
+				throw new Error("Failed to fetch questions");
 			}
-
-			setRandQuestion(res.data[Math.floor(Math.random() * res.data.length)])
-			setStartTime(Date.now());
+			nextQuestion(res.data);
 		} catch (e) {
-			console.error(e)
+			console.error(e);
 		} finally {
-			setIsLoading(false)
+			setIsLoading(false);
 		}
-	}
+	};
 
 	const isValidAnswer = (answer: string): boolean => {
 		if (!answer) {
 			return false;
 		}
-		return normalizeString(answer) === normalizeString((randQuestion?.ans ?? ""));
-	}
 
-	useEffect(() => {
-		fetchQuestions()
-	}, []);
-
-	useEffect(() => {
-		console.log(randQuestion);
-		console.log(averageTime);
-	}, [randQuestion]);
-
-	useEffect(() => {
-		if (isValidAnswer(userAnswer)) {
-			setRoundsPlayed((prev) => prev + 1);
-
-			const endTime = Date.now();
-			const timeTaken = (endTime - startTime!) / 1000;
-
-			setGuessTimes((prev) => {
-				const updatedGuessTimes = [...prev, timeTaken];
-				const newAverageTime = updatedGuessTimes.reduce((acc, curr) => acc + curr, 0) / updatedGuessTimes.length;
-				setAverageTime(newAverageTime);
-				return updatedGuessTimes;
-			});
-
-			nextQuestion();
+		const ans = normalizeString(answer);
+		const correctAns = normalizeString(randQuestion?.ans ?? "");
+		if (stringDifference(ans, correctAns) <= 2) {
+			return true;
 		}
-	}, [userAnswer]);
 
+		return stringPercentageMatch(ans, correctAns, 65);
+	};
 
+	const checkAnswer = (skipped = false) => {
+		if (!randQuestion) return;
 
-	const nextQuestion = () => {
-		setRandQuestion(questions[Math.floor(Math.random() * questions.length)]);
+		if (!userAnswer || userAnswer === "") {
+			skipped = true; // if the useranswer is empty we count it as a skipped question
+		}
+
+		const correct = isValidAnswer(userAnswer);
+		const timeTaken = (Date.now() - startTime) / 1000;
+
+		const score = correct ? 1 : 0;
+		setGameResults((prev) => [
+			...prev,
+			{
+				question: randQuestion.question,
+				userAnswer,
+				correctAnswer: randQuestion.ans,
+				score,
+				skipped
+			},
+		]);
+
+		if (correct) {
+			setRoundsPlayed((prev) => prev + 1);
+		}
+
+		nextQuestion();
+	};
+
+	const nextQuestion = (data = questions) => {
+		if (gameTime <= 0) return;
+
+		setRandQuestion(data[Math.floor(Math.random() * data.length)]);
 		setUserAnswer("");
 		setStartTime(Date.now());
-	}
+	};
 
 	const onUserInput = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setUserAnswer(e.currentTarget.value);
-	}
+	};
 
 	const onSkip = () => {
-		nextQuestion();
+		checkAnswer(true);
+	};
+
+	const restartGame = () => {
+		setGameOver(false);
+		setGameTime(GAME_LENGTH_SECONDS);
+		setGameResults([]);
+
+		setRandQuestion(questions[Math.floor(Math.random() * questions.length)]);
+		setUserAnswer("");
+		setStartTime(Date.now());
+	};
+
+	const getScoreTextNColor = (gameScore: GameResult): string[] => {
+		if (gameScore.skipped) {
+			return ["Pass", "text-[#afaaa9]"]
+		}
+
+		if (gameScore.score === 1) {
+			return ["Rétt", "text-[#00ff00]"]
+		}
+
+		return ["Rangt", "text-[#ff0000]"]
 	}
+
+
+	useEffect(() => {
+		fetchQuestions();
+	}, []);
+
+	useEffect(() => {
+		if (gameTime > 0 && !gameOver) {
+			const timer = setInterval(() => {
+				setGameTime((prevTime) => {
+					if (prevTime <= 1) {
+						clearInterval(timer);
+						setGameOver(true); // Game Over
+						return 0;
+					}
+					return prevTime - 1;
+				});
+			}, 1000);
+
+			return () => clearInterval(timer);
+		}
+	}, [gameTime, gameOver]);
+
+	useEffect(() => {
+		if (!randQuestion) return;
+		setStartTime(Date.now());
+		console.log(randQuestion);
+	}, [randQuestion]);
 
 	return (
 		<section className="mt-8 flex w-full flex-col items-center justify-center">
-			<p className="mb-4 text-center text-lg ">
-				<span className=" font-semibold">Spurning: </span>
-				<br/>
-				<span className={"text-accent"}>{randQuestion?.question ? capitalize(randQuestion?.question) : ""}</span>
-			</p>
-			<div className="container mx-auto grid max-w-2xl items-center justify-center gap-6 pb-8 pt-6 md:py-10">
-				<div className="flex flex-col rounded-md p-8 shadow-lg shadow-black">
-					<label htmlFor="capital-inp" className="mb-2 text-sm text-gray-300">
-						Svar
-					</label>
-					<input
-						id="cap-inp"
-						type="text"
-						value={userAnswer}
-						onChange={onUserInput}
-						className={"textinp"}
-						placeholder="Sláðu inn svar..."
-						autoFocus={true}
-					/>
+			{!gameOver ? (
+				<>
+					<p className="mb-4 text-center text-lg ">
+						<span className="font-semibold">Spurning: </span>
+						<br />
+						<span className={"text-accent"}>
+              {randQuestion?.question ? capitalize(randQuestion?.question) : ""}
+            </span>
+					</p>
+					<div className="container mx-auto grid max-w-2xl items-center justify-center gap-6 pb-8 pt-6 md:py-10">
+						<div className="flex flex-col rounded-md p-8 shadow-lg shadow-black">
+							<label htmlFor="capital-inp" className="mb-2 text-sm text-gray-300">
+								Svar
+							</label>
+							<input
+								id="cap-inp"
+								type="text"
+								value={userAnswer}
+								onChange={onUserInput}
+								className={"textinp"}
+								placeholder="Sláðu inn svar..."
+								autoFocus={true}
+								onKeyUp={(e) => {
+									if (e.key === "Enter") {
+										checkAnswer();
+									}
+								}}
+							/>
+						</div>
+
+						<Button
+							variant={"default"}
+							className="my-2 max-h-12  min-w-28 rounded-md p-2 shadow-md shadow-black"
+							onClick={onSkip}
+						>
+							Næsta
+						</Button>
+
+						<p className="text-md text-foreground">
+							Tími:{" "}
+							<span className="font-semibold text-accent">
+								{gameTime}s
+							</span>
+							{" "} yfir {roundsPlayed} {roundsPlayed === 1 ? "leik" : "leiki"}
+						</p>
+					</div>
+				</>
+			) : (
+				<div className="flex flex-col items-center">
+					<h2 className="text-lg font-semibold">Leikur kláraður!</h2>
+					<p>{gameResults.reduce((p, c) => p + c.score, 0)} stig</p>
+					<div className="mt-4">
+						<h3 className="font-semibold">Úrslit:</h3>
+						<ul>
+							{gameResults.map((result, index) => (
+								<li key={index} className="mb-2">
+									<p>
+										<span className="font-semibold">Spurning: </span>
+										{result.question}
+									</p>
+									<p>
+										<span className="font-semibold">Þitt svar: </span>
+										{result.userAnswer}{" "}
+										<span className={getScoreTextNColor(result)[1]}>
+                      						({getScoreTextNColor(result)[0]}).
+                    					</span>
+									</p>
+									<p>
+										<span className="font-semibold">Rétt svar: </span>
+										{result.correctAnswer}
+									</p>
+								</li>
+							))}
+						</ul>
+					</div>
+
+					<div
+						className={cn(buttonVariants({variant: "default"}), "my-4 hover:cursor-pointer")}
+						onClick={restartGame}
+					>
+						Spila aftur
+					</div>
 				</div>
-
-				<Button
-					variant={"default"}
-					className="my-2 max-h-12  min-w-28 rounded-md p-2 shadow-md shadow-black"
-					onClick={onSkip}
-				>
-					Næsta
-				</Button>
-
-				<p className="text-md text-foreground">
-					Meðaltími:{" "}
-					<span className="font-semibold text-accent">
-					{averageTime ? averageTime.toFixed(2) : "--:--"}
-				  </span>
-					{" "} sekúndur yfir {roundsPlayed} {roundsPlayed === 1 ? 'leik' : 'leiki'}
-				</p>
-			</div>
+			)}
 		</section>
-	)
-}
+	);
+};
 
-export default GeneralQuestions
+export default GeneralQuestions;
--- lib/utils.ts
+++ lib/utils.ts
@@ -29,3 +29,57 @@ export function normalizeString(str: string): string {
 		.replace(/[ýÿ]/g, "y") // Replace accented 'y' characters
 		.replace(/[^a-zA-Z0-9áéíóúýöæðþü\s]/g, ""); // Remove special characters except Icelandic letters and spaces
 }
+
+
+/**
+ * Returns a number which represents the amount of differing characters between the two strings
+ * @param str1
+ * @param str2
+ */
+export function stringDifference (str1: string, str2: string): number {
+	const len = Math.max(str1.length, str2.length);
+	let diffCount = 0;
+
+	for (let i = 0; i < len; i++) {
+		if (str1[i] !== str2[i]) {
+			diffCount++;
+		}
+	}
+
+	return diffCount;
+}
+
+
+/**
+ * Returns whether string a is a percentage-th of string b
+ * @param a
+ * @param b
+ * @param percentage
+ */
+export function stringPercentageMatch (a: string, b: string, percentage: number): boolean  {
+	if (percentage < 0 || percentage > 100) {
+		throw new Error("Percentage must be between 0 and 100.");
+	}
+
+	const normalize = (str: string) => str.replace(/\s+/g, "").toLowerCase(); // Remove spaces and normalize case
+	const aNormalized = normalize(a);
+	const bNormalized = normalize(b);
+
+	let matchingCharacters = 0;
+	const bCharMap: Record<string, number> = {};
+
+	for (const char of bNormalized) {
+		bCharMap[char] = (bCharMap[char] || 0) + 1;
+	}
+
+	for (const char of aNormalized) {
+		if (bCharMap[char]) {
+			matchingCharacters++;
+			bCharMap[char]--;
+		}
+	}
+
+	const percentageMatch = (matchingCharacters / aNormalized.length) * 100;
+
+	return percentageMatch >= percentage;
+};
