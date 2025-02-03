c2cd1a76e233590ced7cec676dc226ff5e44b7e9
cylorun <allidabbi@gmail.com>
2025-02-03 23:06:10 +0000

    supress hydration warnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
 						"min-h-screen bg-background font-sans antialiased",
 						fontSans.variable
 					)}
+					suppressHydrationWarning
 				>
 					<ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
 						<AuthProvider>
