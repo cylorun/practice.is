22ea810448ceb8b56a986be1fd86c8dd6af36444
cylorun <allidabbi@gmail.com>
2025-02-17 21:48:06 +0000

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
 import {AuthProvider} from "@/components/auth-provider";
+import Script from "next/script"
 
 export const metadata: Metadata = {
 	title: {
@@ -28,30 +29,49 @@ interface RootLayoutProps {
 	children: React.ReactNode
 }
 
+const GA_TRACKING_ID = "G-TL447XBT2L";
+
+
 export default function RootLayout({ children }: RootLayoutProps) {
 	return (
 		<>
 			<html lang="en" suppressHydrationWarning>
-				<head />
-				<body
-					className={cn(
-						"min-h-screen bg-background font-sans antialiased",
-						fontSans.variable
-					)}
-					suppressHydrationWarning
+			<head>
+				<Script
+					strategy="afterInteractive"
+					src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+				/>
+				<Script
+					id="google-analytics"
+					strategy="afterInteractive"
 				>
-					<ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-						<AuthProvider>
-							<GameModeProvider>
-								<div className="relative flex min-h-screen flex-col">
-									<SiteHeader />
-									<div className="flex-1">{children}</div>
-								</div>
-								<TailwindIndicator />
-							</GameModeProvider>
-						</AuthProvider>
-					</ThemeProvider>
-				</body>
+					{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', '${GA_TRACKING_ID}');
+				  `}
+				</Script>
+			</head>
+			<body
+				className={cn(
+					"min-h-screen bg-background font-sans antialiased",
+					fontSans.variable
+				)}
+				suppressHydrationWarning
+			>
+			<ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+				<AuthProvider>
+					<GameModeProvider>
+						<div className="relative flex min-h-screen flex-col">
+							<SiteHeader/>
+							<div className="flex-1">{children}</div>
+						</div>
+						<TailwindIndicator/>
+					</GameModeProvider>
+				</AuthProvider>
+			</ThemeProvider>
+			</body>
 			</html>
 		</>
 	)
