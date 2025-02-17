9ddbf3e3dba168a5b3c47884645af6ba0c10de82
cylorun <allidabbi@gmail.com>
2025-02-17 22:56:15 +0000

    fix nav on phone

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -21,10 +21,10 @@ export function SiteHeader() {
 	}
 	return (
 		<header className="sticky top-0 z-40 w-full bg-background ">
-			<div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+			<div className="container flex h-16 items-center">
 				<MainNav/>
-				<div className="flex flex-1 items-center justify-end space-x-4">
-					<nav className="flex items-center space-x-1">
+				<div className="flex flex-1 items-center justify-end space-x-0 sm:space-x-4">
+					<nav className="flex items-center space-x-0">
 						<Link
 							href={"/account"}
 							className={"flex items-center text-sm font-medium"}
